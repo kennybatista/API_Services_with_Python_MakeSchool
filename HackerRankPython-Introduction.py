42ca2c4ff8802e5d125552683efd19025da419fc
Kenny Batista <kennybatista7@gmail.com>
2016-10-12 10:25:46 -0700

    Update HackerRankPython-Introduction.py

--- HackerRankPython-Introduction.py
+++ HackerRankPython-Introduction.py
@@ -2,3 +2,12 @@
 
 my_string = "Hello, World!"
 print(my_string)
+
+
+# Task 
+# Read a line of input from stdin and save it to a variable, . Then print the contents of  to stdout.
+
+# This is stdin
+s = raw_input()
+# This is stdout
+print(s)
